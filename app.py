dddb2ae222e55a6843a9a8d5b29427882d62b224
CynAng <cynthiatakam0@gmail.com>
2025-10-21 09:32:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-
+import pandas as pd
 st.header("mon application")
 
-st.write("Cynthia")
+nombres = [1,2,3,4]
+carré = [1**2,2**2,3**2,4**2]
+
+d = {"nombres": nombres, "carré": carré}
+df=pd.DataFrame(d)
+
+st.write(df)
+
